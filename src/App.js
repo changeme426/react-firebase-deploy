ccfa32200214abc11c68b7dc7232b2bdcd52da86
Full Stack Software Engineer(Web & Mobile) <lovetallman426@gmail.com>
2023-03-29 12:53:28 -0400

    update github action

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React & World Fast Growing Company
         </a>
       </header>
     </div>
